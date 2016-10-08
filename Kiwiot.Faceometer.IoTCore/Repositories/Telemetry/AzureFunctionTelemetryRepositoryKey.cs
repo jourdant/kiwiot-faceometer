84760061ce1261c9d647d4563366ade28028cf87
Jourdan Templeton <j@en.vc>
2016-10-08 20:57:25 +1300

    Updated logic to prompt user for telemetry url User will now be prompted to update functionUrl in AzureFunctionTelemetryRepositoryKey.cs

---
+++ Kiwiot.Faceometer.IoTCore/Repositories/Telemetry/AzureFunctionTelemetryRepositoryKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiwiot.Faceometer.IoTCore.Repositories.Telemetry
+{
+    public sealed partial class AzureFunctionTelemetryRepository : ITelemetryRepository
+    {
+        private string functionUrl => "";
+    }
+}
