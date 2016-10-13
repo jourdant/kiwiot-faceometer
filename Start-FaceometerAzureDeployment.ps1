dc792b55b3f5ce425f948885752b056e5bc64553
Jourdan Templeton <hello@jourdant.me>
2016-10-13 21:40:42 +1300

    Updated Azure deployment script Updated Azure Function code

--- Start-FaceometerAzureDeployment.ps1
+++ Start-FaceometerAzureDeployment.ps1
@@ -16,18 +16,27 @@ If ($sub.Count -ne $null -and $sub.Count -gt 1) {
     $sub = $sub[0]
 }
 
-
-$prov = .\kiwiot.faceometer.azurerm\deploy.ps1 -subscriptionId $sub.subscriptionid -resourceGroupName $ResourceGroupName -resourceGroupLocation $ResourceGroupLocation -deploymentName "faceometer2" -templateFilePath .\kiwiot.faceometer.azurerm\template.json -parametersFilePath .\kiwiot.faceometer.azurerm\parameters.json -Verbose
+"Provisioning Azure Resources"
+"Resource group name: $ResourceGroupName"
+$prov = .\kiwiot.faceometer.azurerm\deploy.ps1 -subscriptionId $sub.subscriptionid -resourceGroupName $ResourceGroupName -resourceGroupLocation $ResourceGroupLocation -deploymentName $ResourceGroupLocation -templateFilePath .\kiwiot.faceometer.azurerm\template.json -parametersFilePath .\kiwiot.faceometer.azurerm\parameters.json
 
 #build kudu url
-"$($prov.outputs.functionAppName.Value)"
+#"$($prov.outputs.functionAppName.Value)"
 
 #build function for deployment object
+"Uploading code into azure function:  $($prov.outputs.functionAppName.Value).azurewebsites.net"
+$functionName = "faceometer_gateway"
+$basePath = ".\Kiwiot.Faceometer.AzureFunctions\$functionName"
 $url = $prov.outputs.functionAppName.Value
-$triggers = get-content .\Kiwiot.Faceometer.AzureFunctions\iot_gateway\function.json | convertfrom-json
-$function = (Get-content ".\Kiwiot.Faceometer.AzureFunctions\iot_gateway\run.csx" -Raw).ToString()
-$testjson = (Get-content ".\Kiwiot.Faceometer.AzureFunctions\iot_gateway\test.json" -Raw).ToString()
-$files = @{"run.csx"=$function}
+
+$triggers = get-content "$basePath\function.json" | convertfrom-json
+$testjson = (Get-content "$basePath\test.json" -Raw).ToString()
+$files = @{"run.csx"=(Get-content "$basePath\run.csx" -Raw).ToString()}
 $prop = New-object PSObject -Prop @{ "config"=$triggers; "files"=$files; "test_data"=$testjson}
 
-New-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType "Microsoft.Web/sites/functions" -ResourceName "$url/iot_gateway" -PropertyObject $prop -ApiVersion 2015-08-01 -Force
+$result = New-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType "Microsoft.Web/sites/functions" -ResourceName "$url/$functionName" -PropertyObject $prop -ApiVersion 2015-08-01 -Force
+
+"Retrieving url trigger URL`r`n`r`n"
+$secrets = Invoke-AzureRmResourceAction -ResourceGroupName $resourcegroupname -ResourceType "Microsoft.Web/sites/functions" -ResourceName "$url/$functionName" -Action listSecrets -ApiVersion "2016-03-01" -Force
+
+$secrets | fl
